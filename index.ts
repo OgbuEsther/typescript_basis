110b79161fcd832696d58204a435e92c3a9b4bd1
OgbuEsther <ogbuuzoma413@gmail.com>
2026-06-12 17:37:03 +0100

    class

--- index.ts
+++ index.ts
@@ -58,3 +58,127 @@ let student: {
   age: 25,
   isEnrolled: true,
 };
+
+
+//types and interfaces
+type User ={
+  name: string;
+  age: number;
+  isEnrolled: boolean;
+}
+
+let user : User = {
+  name : "esther",
+  age : 25,
+  isEnrolled : true,
+}
+
+type VotersInfo = {
+  name : string,
+  age : number,
+  registered : boolean
+}
+
+const voter :VotersInfo = {
+name : "John",
+age : 50,
+registered : true
+}
+
+
+//similarities :
+//used to structure data shape
+type Products = {
+   name : string;
+  price : number;
+  inStock : boolean;
+}
+interface Product {
+  name : string;
+  price : number;
+  inStock : boolean;
+}
+
+//used as function params
+interface greet {
+  name : string;
+}
+type greets = {
+name : string;
+}
+
+function hello(user :greets){
+  console.log(`Hello, ${user.name}!`);
+}
+
+//support optional and readonly parameters
+type UserInfo = {
+  name : string;
+  readonly age : number;
+  email? : string;
+}
+interface UserInfos  {
+  name : string;
+ readonly age : number;
+  email? : string;
+}
+
+//differences 
+//interface can be reused or reopened 
+interface IUser {
+  name : string
+}
+interface IUser{
+  age : number
+
+}
+const obj : IUser ={
+  name : "Esther",
+  age : 25
+}
+
+////type cannot be reopened or reused
+type IUsers = {
+  name : string
+}
+
+
+//type can be used to represent more than objects
+type Name = string
+type Age = number
+type UserDetails = {
+  name : string;
+  age : number;
+}
+type Admin =[string , number]
+type Minus = (a:number ,b:number)=> number
+
+// interface uses extends , type uses intersection 
+interface Parent {
+  height : string;
+  isFemale : boolean;
+}
+interface Child extends Parent {
+  complexion : string
+}
+
+const Jace : Child ={
+  height : "5ft",
+  isFemale : false,
+  complexion : "fair"
+}
+
+type ParentInfo =  {
+    height : string;
+  isFemale : boolean;
+}
+
+type ChildInfo = ParentInfo & {
+  complexion : string
+}
+
+const Esther : ChildInfo ={
+  height : "5'2ft",
+  isFemale : true,
+  complexion : "dark"
+}
